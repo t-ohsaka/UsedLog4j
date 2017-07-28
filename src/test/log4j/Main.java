f0f7af7c080ba55553e77c6b23be8dd8fb7643dc
t-ohsaka <t-ohsaka@plus-bm.jp>
2017-07-28 12:13:58 +0900

    update コンソールの出力のみするように変更した
    まだ、ローリングファイルの方の設定ができていないため

--- src/test/log4j/Main.java
+++ src/test/log4j/Main.java
@@ -5,13 +5,11 @@ import org.apache.log4j.Logger;
 public class Main {
 
 	private static Logger log = Logger.getLogger(Main.class);
-	private static Logger userLog = new UserLog().getLog();
 
 	public static void main(String[] args) throws InterruptedException {
 		while(true) {
 			Thread.sleep(2 * 1000);
 			log.info("Hello Log4j!");
-			userLog.info("Hello UserLog!");
 		}
 	}
 
