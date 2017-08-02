c62646be04d87b4a4db65fb6a1cfbb4fa8aa7d50
t-ohsaka <t-ohsaka@plus-bm.jp>
2017-08-02 14:35:58 +0900

    update UserLogを使用して、ファイルにログを出力できた
    ローリングもできていることが確認できた

--- src/test/log4j/Main.java
+++ src/test/log4j/Main.java
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger;
 
 public class Main {
 
-	private static Logger log = Logger.getLogger(Main.class);
+	private static Logger log = new UserLog().getLog();
 
 	public static void main(String[] args) throws InterruptedException {
 		while(true) {
