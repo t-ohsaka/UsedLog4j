011027595163b11a80e75405c5528ad5695a5ad9
t-ohsaka <t-ohsaka@plus-bm.jp>
2017-08-02 14:45:56 +0900

    update Log4j2を使用して、コンソールとファイルへログを出力できた
    ローリングもできている

--- src/test/log4j/Main.java
+++ src/test/log4j/Main.java
@@ -4,12 +4,14 @@ import org.apache.log4j.Logger;
 
 public class Main {
 
-	private static Logger log = new UserLog().getLog();
+	private static Logger log = Logger.getLogger(Main.class);
+	private static Logger userLog = new UserLog().getLog();
 
 	public static void main(String[] args) throws InterruptedException {
 		while(true) {
 			Thread.sleep(2 * 1000);
-			log.info("Hello Log4j!");
+			log.info("Hello Log4j2!");
+			userLog.info("Hello Log4j2!");
 		}
 	}
 
